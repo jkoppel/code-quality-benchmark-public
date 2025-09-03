4b6fa1f6bbd4fb759736703bcfe6bf604b03a215
ym-han <hanyongming@gmail.com>
2025-09-03 13:33:25 +0800

    Add initial attempt at schema for functionality test suite results

---
+++ src/benchmark-functionality-tests/test-lib/report.ts
@@ -0,0 +1,48 @@
+import { z } from 'zod';
+
+/*********************************
+    For report
+************************************/
+
+/* TODO For the future:
+metadata like CC session id
+*/
+
+export interface TestSuiteResults {
+  name: string;
+  timestamp: string;
+  summary: {
+    total: number;
+    passed: number;
+    failed: number;
+    skipped: number;
+    duration: number;
+  };
+  results: TestResult[]
+}
+
+export const TestResultSchema = z.object({
+  name: z.string(),
+  outcome: z.discriminatedUnion('status', [
+    z.object({
+      status: z.literal('passed'),
+      howTested: z.string().describe('Description of what was done to test this, if not obvious')
+    }),
+    z.object({
+      status: z.literal('failed'),
+      reason: z.string(),
+      howTested: z.string().describe('Description of what was done to test this, if not obvious')
+    }),
+    z.object({
+      status: z.literal('skipped'),
+      reason: z.string()
+    })
+  ])
+});
+
+export type TestResult = z.infer<typeof TestResultSchema>;
+
+// interface TestGroupResults {
+//   name: string;
+//   tests: TestResult[];
+// }
